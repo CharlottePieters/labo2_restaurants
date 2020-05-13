895bfc13bc197de1eec19165581b2275e43af054
Charlotte Pieters <charlotte.pieters@student.ucll.be>
2020-05-13 12:24:58 +0200

    Initial commit

---
+++ src/main/java/be/ucll/labo2_restaurants/Labo2RestaurantsApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.labo2_restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo2RestaurantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labo2RestaurantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ucll/labo2_restaurants/Labo2RestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.labo2_restaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo2RestaurantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
